2f257440cf39254362f7b67124109871e0380719
Yana Rozova <rozova.ys@gmail.com>
2024-07-02 18:28:53 +0300

    Start project

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import EnhancedTable from "../EnhancedTable/EnhancedTable";
+
+function App() {
+  return (
+    <div className="App">
+      <EnhancedTable></EnhancedTable>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/mocks/mocks.js
@@ -0,0 +1,24 @@
+const NAV = {
+
+}
+
+const treadmillsList = [
+  { id: 0, name: 'owl' },
+  { id: 1, name: 'snipe' },
+  { id: 2, name: 'corvus' },
+  { id: 3, name: 'harrier' },
+  { id: 4, name: 'sirena' },
+  { id: 5, name: 'ultima' },
+  { id: 6, name: 'stellar' },
+  { id: 7, name: 'ultima tft' },
+  { id: 8, name: 'merlin a' },
+  { id: 9, name: 'shrike' },
+  { id: 10, name: 'harrier' }
+];
+
+const navs1 = [
+  { nav1: 'беговая дорожка',
+    treadmills:[0,5,6,7,10]
+   }
+]
+
